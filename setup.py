3bdb3a20499c5ad1ac408dd6d77905843fadf703
Filip Sufitchi <filip@scientiamobile.com>
2014-12-15 17:09:55 -0500

    first commit please ignore

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, Extension, find_packages
+
+setup(name='codebox',
+      version='0.1',
+      author='Filip Sufitchi',
+      author_email="fsufitchi@gmail.com",
+      description="Code sandbox using docker.io",
+      url="https://github.com/fsufitch/codebox",
+      packages=find_packages('src'),
+      package_dir={'':'src'},
+      entry_points = {
+        },
+      install_requires=["docker-py"],
+      )
